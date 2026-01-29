484a47c0458bfd0fd7369635788309e3bfe5118e
0x1057801 <0x1057801@gmail.com>
2026-01-29 09:12:36 -0600

    small update giving credit to guy on Reddit for teaching me to use a MockNFT for the purposes of this auction project

--- test/MockNFT.sol
+++ test/MockNFT.sol
@@ -2,6 +2,10 @@ pragma solidity 0.8.33;
 
 import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
+// I can't take credit for this really.  I got this idea from someone on Reddit
+// Figuring out how to get what this provides into my project took FOREVER
+// Thank you anonymous Dev on Reddit!!
+
 contract MockNFT is ERC721 {
 
     // setting up a counter to issue IDs upon minting
